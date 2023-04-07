5deff4361f75dccc039d68ac608f98487bccbbcd
suhrobcoder <suhrobcoder@gmail.com>
2023-04-07 18:33:52 +0500

    initial commit

---
+++ lib/flapp_bird_game.dart
@@ -0,0 +1,8 @@
+import 'dart:async';
+
+import 'package:flame/game.dart';
+
+class FlappyBirdGame extends FlameGame {
+  @override
+  FutureOr<void> onLoad() {}
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flame/game.dart';
+import 'package:flame_example/flapp_bird_game.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(GameWidget(game: FlappyBirdGame()));
+}
